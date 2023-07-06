86502ff41b542ef5e79320a849c30dbf48e1e652
Prateek Kishore Karnad <trainer.it.ssk@gmail.com>
2023-07-06 15:11:24 +0530

    first commit

---
+++ src/main/java/com/example/gatewayserver/GatewayserverApplication.java
@@ -0,0 +1,15 @@
+package com.example.gatewayserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class GatewayserverApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayserverApplication.class, args);
+    }
+
+}
